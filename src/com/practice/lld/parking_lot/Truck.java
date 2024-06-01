62ff2a3b68b60ff72050018455659851aed1ad90
Naman Gulati <naman.gulati@loconav.com>
2024-06-01 21:06:46 +0530

    update git ignore

---
+++ src/com/practice/lld/parking_lot/Truck.java
@@ -0,0 +1,8 @@
+package com.practice.lld.parking_lot;
+
+public class Truck extends  Vehicle {
+
+  public Truck(String registrationNumber) {
+    super(registrationNumber, VehicleType.TRUCK);
+  }
+}
