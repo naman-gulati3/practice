3c13d678508068f25c6ec7321e6618137303f7c3
Naman Gulati <40243817+naman-gulati3@users.noreply.github.com>
2024-09-13 19:22:08 +0530

    Add House Robber

---
+++ src/com/practice/dsa/dp/HouseRobber1.java
@@ -0,0 +1,25 @@
+class HouserRobber {
+ public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        int n = nums.length;
+        Arrays.fill(dp, -1);
+
+        return  solve(nums, 0, dp);
+    }
+
+    private int solve(int[] nums, int i, int[] dp) {
+        if(i >= nums.length) {
+            return 0;
+        }
+
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+
+        int notRob = solve(nums, i + 1, dp);
+
+        int rob = nums[i] + solve(nums, i + 2, dp);
+
+        return dp[i] = Math.max(notRob, rob); 
+    }
+}
