96dc51af7affa3b5ee0a5a2bb596e3ad2521449c
Naman Gulati <40243817+naman-gulati3@users.noreply.github.com>
2024-10-11 20:39:13 +0530

    remove unnecessary loop

--- src/main/java/com/practice/dsa/arrays/ThreeSum.java
+++ src/main/java/com/practice/dsa/arrays/ThreeSum.java
@@ -28,9 +28,6 @@ public class ThreeSum {
           while (j < k && nums[j] == nums[j - 1]) {
             j++;
           }
-          while (j < k && nums[k] == nums[k + 1]) {
-            k--;
-          }
 
         } else if (sum > 0) {
           k--;
