21ada0df1dd4dec0994573affd8e2bd0bc44b5bc
Naman Gulati <naman.gulati@kulu.com>
2025-03-19 13:21:09 +0530

    multithreaded even odd print

---
+++ src/main/java/com/practice/interview_questions/MultiThreadedEvenOddPrint.java
@@ -0,0 +1,64 @@
+package com.practice.interview_questions;
+
+import java.util.concurrent.Semaphore;
+
+public class MultiThreadedEvenOddPrint {
+
+    private static int counter = 1;
+    private static final int MAX_NUM = 1000;
+    private static final Semaphore evenLock = new Semaphore(1);
+    private static final Semaphore oddLock = new Semaphore(0);
+
+    static class NumPrinter
+            implements Runnable {
+
+        private final boolean isEven;
+
+        public NumPrinter(boolean isEven) {
+            this.isEven = isEven;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    if (isEven) {
+                        evenLock.acquire();
+                    } else {
+                        oddLock.acquire();
+                    }
+
+                    if (counter > MAX_NUM) {
+                        oddLock.release();
+                        evenLock.release();
+                        break;
+                    }
+
+                    System.out.println(counter++);
+                    if (isEven) {
+                        oddLock.release();
+                    } else {
+                        evenLock.release();
+                    }
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        var t1 = new Thread(() -> new NumPrinter(true).run(),
+                            "even");
+        var t2 = new Thread(() -> new NumPrinter(false).run(),
+                            "odd");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+}
--- src/main/java/com/practice/priority_queue/KthLargestInArray.java
+++ src/main/java/com/practice/priority_queue/KthLargestInArray.java
@@ -5,20 +5,23 @@ import java.util.PriorityQueue;
 
 public class KthLargestInArray {
 
-  public static int findKthLargest(int[] nums, int k) {
-    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-    for (int num : nums) {
-      pq.add(num);
+    public static int findKthLargest(int[] nums,
+                                     int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            pq.add(num);
+        }
+        System.out.println(pq);
+        while (k - 1 > 0) {
+            k--;
+            pq.poll();
+        }
+        return pq.peek();
     }
-    System.out.println(pq);
-    while (k - 1 > 0) {
-      k--;
-      pq.poll();
-    }
-    return pq.peek();
-  }
 
-  public static void main(String[] args) {
-    System.out.println(findKthLargest(new int[] {3, 2, 1, 5, 6, 4}, 2));
-  }
+    public static void main(String[] args) {
+        System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4},
+                                          2));
+    }
+    // 1, 2, 3, 4, 5, 6
 }
